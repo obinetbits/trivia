f2802c92c84853681359b6f0d23c1cc74ae62ddc
Ifeanyi Obiano <obi.netbits@outlook.com>
2016-10-24 15:21:24 +0200

    code refactor - switch statment

--- javascript/game.js
+++ javascript/game.js
@@ -18,37 +18,32 @@ exports.Game = function() {
     return !(purses[currentPlayer] == 6)
   };
 
-  var currentCategory = function(){
-    if(places[currentPlayer] == 0)
-      return 'Pop';
-    if(places[currentPlayer] == 4)
-      return 'Pop';
-    if(places[currentPlayer] == 8)
-      return 'Pop';
-    if(places[currentPlayer] == 1)
-      return 'Science';
-    if(places[currentPlayer] == 5)
-      return 'Science';
-    if(places[currentPlayer] == 9)
-      return 'Science';
-    if(places[currentPlayer] == 2)
-      return 'Sports';
-    if(places[currentPlayer] == 6)
-      return 'Sports';
-    if(places[currentPlayer] == 10)
-      return 'Sports';
-    return 'Rock';
-  };
+  
 
-  this.createRockQuestion = function(index){
-    return "Rock Question "+index;
-  };
+  var currentCategory = function(){
+        switch (places[currentPlayer]) {
+            case 0:
+            case 4:
+            case 8:
+                return 'Pop';
+            case 1:
+            case 5:
+            case 9:
+                return 'Science';
+            case 2:
+            case 6:
+            case 10:
+                return 'Sports';
+            default:
+                return 'Rock';
+        }
+    }
 
   for(var i = 0; i < 50; i++){
     popQuestions.push("Pop Question "+i);
     scienceQuestions.push("Science Question "+i);
     sportsQuestions.push("Sports Question "+i);
-    rockQuestions.push(this.createRockQuestion(i));
+    rockQuestions.push("Rock Question "+i);
   };
 
   this.isPlayable = function(howManyPlayers){
