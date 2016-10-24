18d01f0a56480e9c443ca0e1cc8da0150be6e1e2
Ifeanyi Obiano <obi.netbits@outlook.com>
2016-10-24 07:10:09 +0200

    code refactor

--- test/gameRunner-test.js
+++
@@ -1,8 +0,0 @@
-var expect = require('expect');
-var gameRunner = require('../app/gameRunner');
-
-describe('gameRunner', function(){
-    it('should exist', function(){
-        expect(gameRunner).toExist;
-    });
-});
